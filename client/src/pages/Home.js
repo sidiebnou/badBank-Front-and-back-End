4fa7f459699f43916b908c95f2d3be5ce8d27394
Sidi Mohamed Ebnou <37393021+sidiebnou@users.noreply.github.com>
2023-11-21 16:05:09 -0500

    fix

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,17 +1,23 @@
 import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
 import img from "../bank.png";
 
 export default function Home() {
   return (
-    <Card style={{ width: "500px", height: "600px", margin: 100 }} className="">
-      <Card.Body>
-        <p class="h4">WELCOME TO THE BANK</p>
-        <p class="">For all your banking needs</p>
-        <Card.Img
-          style={{ width: "400px", padding: "40px" }}
-          src={img}
-        ></Card.Img>
-      </Card.Body>
-    </Card>
+    <Container
+      className="mt-5"
+      style={{ display: "flex", justifyContent: "center" }}
+    >
+      <Card style={{ width: "500px" }}>
+        <Card.Body>
+          <p class="h4">WELCOME TO THE BANK</p>
+          <p class="">For all your banking needs</p>
+          <Card.Img
+            style={{ width: "400px", padding: "40px" }}
+            src={img}
+          ></Card.Img>
+        </Card.Body>
+      </Card>
+    </Container>
   );
 }
